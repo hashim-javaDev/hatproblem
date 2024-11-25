9cae70a4fb9b5cf200f96ab6d6156eed8eb4b71f
Hashim <hashimmohammed212@gmail.com>
2024-11-26 00:38:02 +0530

    port 3000

--- server.js
+++ server.js
@@ -80,6 +80,6 @@ io.on('connection', (socket) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
