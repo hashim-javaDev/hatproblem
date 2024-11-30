dd578a6e1fe6db6ba42f9d8c241235fb9aacaa59
Hashim <hashimmohammed212@gmail.com>
2024-11-30 22:13:16 +0530

    port change

--- server.js
+++ server.js
@@ -122,6 +122,6 @@ io.on('connection', (socket) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
